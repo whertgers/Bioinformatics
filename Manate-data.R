a92b11bab247fb6714b19a0f4360b89bad6a57d6
whertgers <158228588+whertgers@users.noreply.github.com>
2024-02-06 18:50:02 -0500

    Add files via upload

---
+++ Manate-data.R
@@ -0,0 +1,11 @@
+library(Biostrings)
+download.file("C:\Users\whert\Downloads\Manatee Gene\Manatee1.fasta")
+genome = readDNAStringSet("Manatee1.fasta")
+readDNAStringSet("Manatee5.fasta")
+fasta.seqlengths("Manatee5.fasta")
+print(,show="complete")
+c("Manatee1.fasta,Manatee2.fasta,Manatee3.fasta,Manatee4.fasta,Manatee5.fasta")
+combine("Manatee1.fasta,Manatee2.fasta,Manatee3.fasta,Manatee4.fasta,Manatee5.fasta")
+library(seqi
+        as.matrix(d)[1:5, "Manatee.fasta", drop=FALSE]
+        
