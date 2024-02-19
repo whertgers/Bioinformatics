3debbde8c639972287487e8f9ac38f73e1d5ac23
whertgers <158228588+whertgers@users.noreply.github.com>
2024-02-18 23:08:23 -0500

    Add files via upload

---
+++ Midterm.R
@@ -0,0 +1,20 @@
+# Load in  all the libraries that are needed. 
+library(msa)
+library(seqinr)
+library(Biostrings)
+library(UniprotR)
+library(protti)
+library(tidyr)
+library(dplyr)
+setwd("/Users/whert/Documents/GitHub/Bioinformatics")
+readDNAStringSet("sequences.fasta")
+genome = readDNAStringSet("sequences.fasta")
+read.fasta("sequences.fasta")
+readDNAStringSet("sequences.fasta")
+
+
+
+
+
+            
+
